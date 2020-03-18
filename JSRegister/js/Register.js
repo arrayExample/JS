6bab30abc85d30cc1a2fee216b2bc001ba257af2
arrayExample <Lippincottk@yahoo.com>
2020-03-18 17:04:18 -0500

    JSON to Created Table in Modal Testing

--- JSRegister/js/Register.js
+++ JSRegister/js/Register.js
@@ -4,15 +4,53 @@ var newCell = newRow.insertCell(0);
 var newText = document.createTextNode('new row');
 newCell.appendChild(newText);
 
-var descriptionData = document.getElementById('uPCData');
+var descriptionData = document.getElementById('table');
 var list = [
- { "UPC": "0001", "Description": "Potatoes", "Price": "$13.22"},
-  { "UPC":"0002","Description":"Cake","Price":"$20.22"},
-   {"UPC":"0003","Description":"Chargine Cable","Price":"$12.20"}
+ { "UPC": "0001", "Description": "Potatoes", "Price": "$13.22" },
+ { "UPC":"0002","Description":"Cake","Price":"$20.22" },
+ { "UPC":"0003","Description":"Chargine Cable","Price":"$12.20" }
 ];
-descriptionData.innerHTML = JSON.stringify(list[0]) + "<br>"
-                + JSON.stringify(list[1]) + "<br>"
+descriptionData.innerHTML = JSON.stringify(list[0]) + "<td>"
+                + JSON.stringify(list[1]) + "<td>"
                 + JSON.stringify(list[2]);
+
+/* Test 1 begin
+let mountains = [
+  { name: "Monte Falco", height: 1658, place: "Parco Foreste Casentinesi" },
+  { name: "Monte Falterona", height: 1654, place: "Parco Foreste Casentinesi" },
+  { name: "Poggio Scali", height: 1520, place: "Parco Foreste Casentinesi" },
+  { name: "Pratomagno", height: 1592, place: "Parco Foreste Casentinesi" },
+  { name: "Monte Amiata", height: 1738, place: "Siena" }
+];
+
+function generateTableHead(table, data) {
+  let thead = table.createTHead();
+  let row = thead.insertRow();
+  for (let key of data) {
+    let th = document.createElement("th");
+    let text = document.createTextNode(key);
+    th.appendChild(text);
+    row.appendChild(th);
+  }
+}
+
+function generateTable(table, data) {
+  for (let element of data) {
+    let row = table.insertRow();
+    for (key in element) {
+      let cell = row.insertCell();
+      let text = document.createTextNode(element[key]);
+      cell.appendChild(text);
+    }
+  }
+}
+
+let table = document.querySelector("table");
+let data = Object.keys(mountains[0]);
+generateTableHead(table, data);
+generateTable(table, mountains);
+
+  Test 1 End*/
 
 function constructTable(selector) {
     // Getting the all column names
@@ -42,7 +80,7 @@ function Headers(list, selector) {
             if ($.inArray(k, columns) == -1) {
                 columns.push(k);
                 // Creating the header
-                header.append($('<th/>').html(k));
+                header.append($('<td/>').html(k));
             }
         }
     }
@@ -53,12 +91,20 @@ function Headers(list, selector) {
 
 var modalButtons = [...document.querySelectorAll(".button")];
 modalButtons.forEach(function(btn){
+    btn.onclick = function() {
+        var modal = btn.getAttribute('data-modal');
+        document.getElementById(modal).style.display = "block";
+    }
+});
+/*
+var modalSelectButtons = [...document.querySelectorAll(".button")];
+modalSelectButtons.forEach(function(btn){
   btn.onclick = function() {
     var modal = btn.getAttribute('data-modal');
     document.getElementById(modal).style.display = "block";
   }
 });
-
+*/
 var closeButtons = [...document.querySelectorAll(".close")];
 closeButtons.forEach(function(btn){
   btn.onclick = function() {
