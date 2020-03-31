e5ea9572b25287103fd9eee864b11c4a3fa2ff5c
arrayExample <Lippincottk@yahoo.com>
2020-03-31 07:53:30 -0500

    Table creation.

--- JSVue/js/project.js
+++ JSVue/js/project.js
@@ -1,67 +1,45 @@
-var app = new Vue({
+new Vue({
     el: '#app',
     data: {
-        message: 'Hello Vue!'
-    }
-})
-
-var app2 = new Vue({
-    el: '#app-2',
-    data: {
-        message: 'You loaded this page on ' + new Date().toLocaleString()
-    }
-})
-
-  var app3 = new Vue({
-    el: '#app-3',
-    data: {
-        seen: true
-    }
-})
-
-  var app4 = new Vue({
-    el: '#app-4',
-    data: {
-        todos: [
-            { text: 'Learn JavaScript' },
-            { text: 'Learn Vue' },
-            { text: 'Build something awesome' }
-        ]
-    }
-})
-
-  var app5 = new Vue({
-    el: '#app-5',
-    data: {
-        message: 'Hello Vue.js!'
+        counter: 0,
+        x: 0,
+        y: 0
     },
     methods: {
-        reverseMessage: function () {
-            this.message = this.message.split('').reverse().join('')
-      }
-    }
-})
+        decrease: function(){
+            this.counter--;
+        },
+        increase: function(){
+            this.counter++;
+        },
 
-var app6 = new Vue({
-    el: '#app-6',
-    data: {
-        message: 'Hello Vue!'
-    }
-  })
+        updateCoordinates: function(event){
+            this.x = event.clientX;
+            this.y = event.clientY;
+        },
 
-// Define a new component called todo-item
-Vue.component('todo-item', {
-    props: ['todo'],
-    template: '<li>{{ todo.text }}</li>'
-  })
-  
-var app7 = new Vue({
-    el: '#app-7',
-    data: {
-        groceryList: [
-            { id: 0, text: 'Vegetables' },
-            { id: 1, text: 'Cheese' },
-            { id: 2, text: 'Whatever else humans are supposed to eat' }
-        ]
+        enterPressed: function() {
+            alert('The Enter Button was pressed.')
+        },
+    /* CONVERT to? 
+        function saveToFile(jsonData, filename) {
+            let blob = new Blob([jsonData], { type: 'text/plain;charset=utf-8;' })
+            if (navigator.msSaveBlob) { // IE 10+
+              navigator.msSaveBlob(blob, filename)
+            } else {
+              let link = document.createElement('a')
+              if (link.download !== undefined) { // feature detection
+                // Browsers that support HTML5 download attribute
+                let url = URL.createObjectURL(blob)
+                link.setAttribute('href', url)
+                link.setAttribute('download', filename)
+                link.style.visibility = 'hidden'
+                document.body.appendChild(link)
+                link.click()
+                document.body.removeChild(link)
+              }
+            }
+          }
+          */
     }
-})
+});
